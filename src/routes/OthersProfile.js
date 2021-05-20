db1d7045ffb5f0c893638779617340d57e82a18f
sudo-Terry <76080411+sudo-Terry@users.noreply.github.com>
2021-05-20 16:42:37 +0900

    hooks on params

--- src/routes/OthersProfile.js
+++ src/routes/OthersProfile.js
@@ -2,15 +2,16 @@ import React, { useEffect, useState } from 'react';
 import Profile from './Profile';
 import { dbService } from 'myBase';
 import Loader from "react-loader-spinner";
+import { useParams } from 'react-router';
 //============= 추후 Profile.js와 통합 ==============//
 
-function OthersProfile({ match }) {
+function OthersProfile() {
   const [isLoading, setIsLoading] = useState(false);
   const [otherUserObj, setOtherUserObj] = useState({});
 
   const getUserInfo = async () => { //db에서 유저정보 가져오기
     setIsLoading(true);
-    const { userId } = match.params;
+    const { userId } = useParams();
     try{
       const docRef = await dbService.collection("users").doc(`${userId}`);
       const docInfo = await docRef.get();
