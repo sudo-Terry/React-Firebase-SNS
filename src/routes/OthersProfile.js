216d766cc06466ddfaaa6a1eba817e37c11c1ca8
sudo-Terry <76080411+sudo-Terry@users.noreply.github.com>
2021-06-04 15:06:14 +0900

    fix error in OthersProfile.js by useParams

--- src/routes/OthersProfile.js
+++ src/routes/OthersProfile.js
@@ -2,26 +2,26 @@ import React, { useEffect, useState } from 'react';
 import Profile from './Profile';
 import { dbService } from 'myBase';
 import Loader from "react-loader-spinner";
-import { useParams } from 'react-router';
+import { useParams } from 'react-router-dom';
 //============= 추후 Profile.js와 통합 ==============//
 
 function OthersProfile() {
   const [isLoading, setIsLoading] = useState(false);
   const [otherUserObj, setOtherUserObj] = useState({});
+  const { userId } = useParams();
 
   const GetUserInfo = async () => { //db에서 유저정보 가져오기
+
     setIsLoading(true);
-    const { userId } = useParams();
     try{
       const docRef = await dbService.collection("users").doc(`${userId}`);
       const docInfo = await docRef.get();
       setOtherUserObj(docInfo.data());
-      console.log(otherUserObj);
     } catch (err){
       console.log({err});
     }
     setIsLoading(false);
-  }
+  };
 
   useEffect(() => {  
     GetUserInfo();
