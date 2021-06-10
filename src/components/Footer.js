4898e54eea894f4e41eabc33ea5a496053e54c69
sudo-Terry <76080411+sudo-Terry@users.noreply.github.com>
2021-06-10 16:46:06 +0900

    refactor Footer to styled-component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,62 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import React from 'react';
+import styled from 'styled-components';
+
+const FooterContainer = styled.div`
+  width: 350px;
+  height: 100vh;
+  margin-left: 30px;
+`
+
+const FooterSearchWrapper = styled.div``
+
+const FooterSearchBox = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 12px 0px;
+  width: 100%;
+  background-color: #eee;
+  border-radius: 9999px;
+  padding: 12px;
+`
+
+const FooterInput = styled.input`
+  width: 100%;
+  margin-left: 14px;
+`
+
+const FooterContentWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 80%;
+  background-color: #eee;
+  border-radius: 20px;
+`
+
+const FooterContentTitle = styled.h1`
+  width: 100%;
+  padding: 12px 16px;
+  font-size: 18px;
+  font-weight: bold;
+`
 
 function Footer(props) {
   return (
-    <div className="footer-container">
-      <div className="footer-searchbox">
-      <FontAwesomeIcon icon={faSearch} color={"#888"} size="lg" />
-        <input 
-          type="text" 
-          placeholder="크위터 검색"
-          className="footer-search"
-        />
-      </div>
-      <div className="footer-contentbox">
-        <div className="footer-contenttitle">
-          <span>내가 좋아할만한 컨텐츠</span>
-        </div> 
-      </div>  
-    </div>
+    <FooterContainer>
+      <FooterSearchWrapper>
+        <FooterSearchBox>
+          <FontAwesomeIcon icon={faSearch} color={"#888"} size="lg" />
+          <FooterInput 
+            type="text" 
+            placeholder="크위터 검색"
+          />
+        </FooterSearchBox>
+      </FooterSearchWrapper>
+      <FooterContentWrapper>
+        <FooterContentTitle>내가 좋아할만한 컨텐츠</FooterContentTitle> 
+      </FooterContentWrapper>  
+    </FooterContainer>
   );
 }
 
